2f78a80ae589b0dde74b1bebbeb0a71f2a514dac
kartikpavan <kartik.varanasi@yahoo.com>
2022-03-06 23:16:44 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import {
+  FaEnvelopeOpen,
+  FaUser,
+  FaCalendarTimes,
+  FaMap,
+  FaPhone,
+  FaLock,
+} from 'react-icons/fa';
+import Loading from './components/Loading';
+
+const url = 'https://randomuser.me/api/';
+const defaultImage =
+  'https://i.pinimg.com/originals/2b/02/15/2b02159fee58d573c079ad5212d56b63.gif';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [person, setPerson] = useState(null);
+  const [title, setTitle] = useState('name');
+  const [value, setValue] = useState('random Person');
+
+  const getPerson = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    const person = data.results[0];
+    const { email, phone } = person;
+    const { large: image } = person.picture;
+    const { password } = person.login;
+    const { first, last } = person.name;
+    const { age } = person.dob;
+    const {
+      street: { number, name },
+    } = person.location;
+    const newPerson = {
+      image,
+      email,
+      phone,
+      age,
+      password,
+      street: `${number} ${name}`,
+      name: `${first} ${last}`,
+    };
+    setPerson(newPerson);
+    setLoading(false);
+    setTitle('name');
+    setValue(newPerson.name);
+  };
+
+  const handleValue = (e) => {
+    if (e.target.classList.contains('icon')) {
+      const newValue = e.target.dataset.label;
+      setTitle(newValue);
+      setValue(person[newValue]);
+    }
+  };
+
+  const clickHandler = () => {
+    setLoading(true);
+    getPerson();
+  };
+
+  useEffect(() => {
+    getPerson();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="block bcg-black"></div>
+      <div className="block">
+        <div className="container">
+          <img
+            src={(person ? person.image : null) || defaultImage}
+            alt="randdom_img"
+            className="user-img"
+          />
+          <p className="user-title">My {title} is </p>
+          <p className="user-value">{value}</p>
+          <div className="values-list">
+            <button
+              className="icon"
+              data-label="name"
+              onMouseOver={handleValue}
+            >
+              <FaUser />
+            </button>
+            <button
+              className="icon"
+              data-label="email"
+              onMouseOver={handleValue}
+            >
+              <FaEnvelopeOpen />
+            </button>
+            <button className="icon" data-label="age" onMouseOver={handleValue}>
+              <FaCalendarTimes />
+            </button>
+            <button
+              className="icon"
+              data-label="street"
+              onMouseOver={handleValue}
+            >
+              <FaMap />
+            </button>
+            <button
+              className="icon"
+              data-label="phone"
+              onMouseOver={handleValue}
+            >
+              <FaPhone />
+            </button>
+            <button
+              className="icon"
+              data-label="password"
+              onMouseOver={handleValue}
+            >
+              <FaLock />
+            </button>
+          </div>
+          <button className="btn" type="button" onClick={clickHandler}>
+            {loading ? 'Loading...' : 'randomUser'}
+          </button>
+        </div>
+      </div>
+    </main>
   );
 }
 
---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Spinner from '../asset/spinner.gif';
+
+const Loading = () => {
+  return (
+    <div>
+      <Spinner />
+    </div>
+  );
+};
+
+export default Loading;
